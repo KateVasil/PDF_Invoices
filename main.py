8cf53f8520a8b2d03ef9f0280daf063ad73b31ea
Katerina <katevasil2@gmail.com>
2023-04-15 11:38:55 +0300

    Day 25: Add header, total price, company and logo

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ for filepath in filepaths:
     columns = df.columns
     columns = [item.replace("_", " ").title() for item in columns]
 
-    pdf.set_font(family="Times",style="B", size=11)
+    pdf.set_font(family="Times", style="B", size=11)
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
     pdf.cell(w=60, h=8, txt=columns[1], border=1)
@@ -42,5 +42,23 @@ for filepath in filepaths:
         pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", style="B", size=14)
+    pdf.cell(w=25, h=8, txt="PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
